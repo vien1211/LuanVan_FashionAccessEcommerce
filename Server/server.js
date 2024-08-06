6f2a0038e22e7235a24698e57f13875d26b6602a
vien1211 <truongngocvien1211@gmail.com>
2024-08-06 18:15:16 +0700

    06/08

---
+++ Server/server.js
@@ -0,0 +1,14 @@
+const express = require("express")
+
+require('dotenv').config()
+
+const app = express()
+const port = process.env.PORT || 8888
+app.use(express.json())
+app.use(express.urlencoded({extended : true}))
+
+app.use('/', (req, res) => {res.send('Server on')})
+
+app.listen(port, () => {
+    console.log('Server is running on port:', port)
+})
